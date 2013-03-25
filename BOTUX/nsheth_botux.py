c7e0deff4ede07ccb871e750f35f1a3f2ec12d16
Nihar <nsheth@vcu.edu>
2013-03-25 14:18:49 -0400

    Nihars BOTUX program added

---
+++ BOTUX/nsheth_botux.py
@@ -0,0 +1,12 @@
+__author__ = 'Nihar Sheth'
+
+import argparse
+import os.path
+import sys
+
+
+SEED_FILE = 'OTU_seed.txt'
+FREQ_FILE = 'OTU_frequency.txt'
+ASS_FILE = 'OTU_Assignment.txt'
+WORD_FILE = 'OTU_word.txt'
+freqs = {}
