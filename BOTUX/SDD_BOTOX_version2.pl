8f6ff01b507ecff3d089613e0c730cdee96c52b9
Danielle-Dodson <dodsonsd@vcu.edu>
2013-03-28 10:04:03 -0300

    Create SDD_BOTOX_version2.pl

---
+++ BOTUX/SDD_BOTOX_version2.pl
@@ -0,0 +1,79 @@
+#! perl
+
+use strict;
+use warnings;
+
+my $input = shift;
+my $output = shift;
+
+if(!defined($output))
+{
+die "You did not enter a correct input or output file: $!\n"
+}
+  open IN, "<$input";
+open OUT, ">$output";
+
+
+my %Seq_Hash;
+my %seq;
+my %seq_length;
+my @sorted_array_of_hash_values;
+my ($line, $SeqName, $seq, $seq_length);  
+my $num_of_reads = 1;
+my $uniq = 0;
+
+$line = <IN>;
+chomp $line;
+
+$seq = "";
+
+while($line = <IN>) {	
+	if ($line =~ /^>(\w+)\|/) {
+	$num_of_reads ++;
+		if($line ne "") {
+			
+		
+	$SeqName = $1;
+	$line = "";
+	}
+	}
+	else {
+		chomp $line;
+		$seq = $line; # sequence can be on multiple lines
+		#print OUT $seq
+		$seq_length = length($seq);
+			
+				 if(exists $Seq_Hash{$seq_length}{$seq}){
+					$Seq_Hash{$seq_length}{$seq}{count} +=1;
+					
+					}
+				 else{
+					 $Seq_Hash{$seq_length}{$seq}{count} = 1;
+					 $uniq ++;
+					 
+				 }	
+	print OUT ($seq_length, "\t", $seq, "\n");
+		 }
+		
+}
+
+foreach $seq_length ( sort (keys (%Seq_Hash))) { #sorts by length
+	foreach my $seq (sort (keys ($Seq_Hash{$seq_length}) ) ){
+	my $abundance = ($Seq_Hash{$seq_length}{$seq}{count});
+
+	print OUT ("Seq Length:", $seq_length, "\t", "Abundance:", $abundance, "\n");
+	}
+}
+
+
+print OUT ("Total Number of Reads In File:", $num_of_reads, "\n");
+print OUT ("Total Number of Unique Reads:", $uniq );
+
+
+close IN;
+close OUT;	
+
+exit;
+	
+
+
