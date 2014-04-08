df0e55635e5feef062f56ec8c7a10d2772255d67
Bhatas <bhatas@vcu.edu>
2014-04-08 00:04:21 -0400

    Taxonomy
    Script to create the table for taxonomy

---
+++ MicrobiomeDB/archana/Taxonomy.py
@@ -0,0 +1,61 @@
+import argparse
+import re
+
+# Construct ArgumentParser and add the arguments
+parser = argparse.ArgumentParser(description= "Extracts informations from the names and nodes")
+parser.add_argument("file_names", type=str, help= "Input names file")
+parser.add_argument("file_nodes", type=str, help= "Input nodes file")
+
+args = parser.parse_args()
+
+# Creating a file handle to read the information from names file.
+try:
+    fileHandle_1 = open(str(args.file_names), "r")
+except:
+    print "File cannot be opened for reading."
+
+
+# Creating a file handle to read the information from nodes file.
+try:
+    fileHandle_2 = open(str(args.file_nodes), "r")
+except:
+    print "File cannot be opened for reading."
+
+
+# Creating files to write into.
+try:
+    fileOut_1 = open("names_out.txt", "w")
+except:
+    print "File cannot be opened for writing."
+
+header_1 = "Tax_id" + "\t" + "Unique_name" + "\n"
+fileOut_1.write(header_1)
+
+try:
+    fileOut_2 = open("nodes_out.txt", "w")
+except:
+    print "File cannot be opened for writing."
+
+header_2 = "Tax_id" + "\t" + "Parent_tax_id" + "\t" + "rank" + "\n"
+fileOut_2.write(header_2)
+
+
+# Processing the files
+for line in fileHandle_1:
+    #print line
+    columns = line.split("|")
+    tax_id = columns[0]
+    unique_name = columns[2]
+    fileOut_1.write(tax_id + "\t" + unique_name + "\n")
+
+
+for line in fileHandle_2:
+    #print line
+    columns = line.split("|")
+    tax_id = columns[0]
+    parent = columns[1]
+    rank = columns[2]
+    fileOut_2.write(tax_id + "\t" + parent + "\t" + rank + "\n")    
+    
+    
+
